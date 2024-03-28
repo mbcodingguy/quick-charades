875d98e541f6ce1999bb0eb5ae8f7bd137445015
Matthew Brundage <mbrundage86@gmail.com>
2024-03-27 22:18:39 -0500

    Fix relative path for images

--- src/ImageManager.js
+++ src/ImageManager.js
@@ -1,15 +1,15 @@
 
 let images = [
-  {"name":"Monkey","pic":"/animals/monkey.png"},
-  {"name":"Chicken","pic":"/animals/chicken.png"},
-  {"name":"Bird","pic":"/animals/bird.png"},
-  {"name":"Penguin","pic":"/animals/penguin.png"},
-  {"name":"Bunny","pic":"/animals/bunny.png"},
-  {"name":"Fish","pic":"/animals/fish.png"},
-  {"name":"Cat","pic":"/animals/cat.png"},
-  {"name":"Dog","pic":"/animals/dog.png"},
-  {"name":"Horse","pic":"/animals/horse.png"},
-  {"name":"Lion","pic":"/animals/lion.png"}
+  {"name":"Monkey","pic":"./animals/monkey.png"},
+  {"name":"Chicken","pic":"./animals/chicken.png"},
+  {"name":"Bird","pic":"./animals/bird.png"},
+  {"name":"Penguin","pic":"./animals/penguin.png"},
+  {"name":"Bunny","pic":"./animals/bunny.png"},
+  {"name":"Fish","pic":"./animals/fish.png"},
+  {"name":"Cat","pic":"./animals/cat.png"},
+  {"name":"Dog","pic":"./animals/dog.png"},
+  {"name":"Horse","pic":"./animals/horse.png"},
+  {"name":"Lion","pic":"./animals/lion.png"}
 ]
 let currentIndex = -1;
 let randomImages = images.map(i => i.name);
